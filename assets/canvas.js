e145e7528f4bf148d7b22cb751a4050ac7b18db9
ZGQ Inc. <zgqinc@gmail.com>
2026-01-10 23:40:00 +0800

    fix

--- assets/canvas.js
+++ assets/canvas.js
@@ -66,7 +66,7 @@
 
     function draw() {
         ctx.globalCompositeOperation = 'destination-out';
-        ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+        ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
         ctx.fillRect(0, 0, width, height);
 
         ctx.globalCompositeOperation = 'source-over';
